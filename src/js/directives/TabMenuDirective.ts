774c986799bafe23b54919afddbf00b73d33bd55
Jonathan Imson <jymysyn@gmail.com>
2017-03-23 00:38:13 -0700

    show about tab on profile page load, center profile page header

--- src/js/directives/TabMenuDirective.ts
+++ src/js/directives/TabMenuDirective.ts
@@ -1,15 +1,7 @@
 angular.module('app').directive('tabMenu', () => ({
   restrict: 'A',
   link: function (scope, elem, attrs) {  
-    $(".menu>[data-tab=first]").tab({
-      // apiSettings: {
-      //   loadingDuration: 3000,
-      //   mockResponse: function(settings) {
-      // 
-      //     return "This is response for tab first";
-      //   }
-      // }
-    });
+    $(".menu>[data-tab=first]").tab();
     $(".menu>[data-tab=second]").tab()
     $(".menu>[data-tab=third]").tab()
   }
